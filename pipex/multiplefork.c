25b1836479a4bfc7b358ce97ce6d74789a5fc51a
Denis Lom <lom.denis@seznam.cz>
2023-08-06 17:50:17 +0200

    explanation in commentary added

--- pipex/multiplefork.c
+++ pipex/multiplefork.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 00:37:19 by dlom              #+#    #+#             */
-/*   Updated: 2023/08/03 00:49:47 by dlom             ###   ########.fr       */
+/*   Updated: 2023/08/06 17:49:15 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,23 @@ programming handbook. San Francisco: No Starch Press, c2010.
 ISBN 978-1-59327-220-3.
 [2]
 https://www.youtube.com/watch?v=tcYo6hipaSA&list=PLfqABt5AS4FkW5mOn2Tn9ZZLLDwA3kZUY&index=5
-	__________
-	|id1 = 
-	|id2 =
-	|_________|
+Calling fork two times:
+				____________
+				|main pr.	|
+				|id1 = x	|
+				|id2 = z	|
+				|___________|
+			_____|______________
+			|					|
+	____________		____________
+	|id1 = 0	|		|id1 = x	|
+	|id2 = y	|		|id2 = 0	|
+	|____X______|		|____Z______|
+		|
+	____________
+	|id1 = 0	|
+	|id2 = 0	|
+	|____Y______|
 */
 
 int	main(int argc, char *arv[])
