5ee139921dfdb4fb20a1ff27460faf78d4ca6a2e
Denis Lom <lom.denis@seznam.cz>
2023-08-16 22:23:12 +0200

    error checks added

--- pipex/05pipes.c
+++ pipex/05pipes.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 21:24:18 by dlom              #+#    #+#             */
-/*   Updated: 2023/08/15 23:23:58 by dlom             ###   ########.fr       */
+/*   Updated: 2023/08/16 22:22:31 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,18 @@ https://www.youtube.com/watch?v=Mqb2dVRe0uo&list=PLfqABt5AS4FkW5mOn2Tn9ZZLLDwA3k
 		RETURNS: 0 if succesful, -1 if not
 	fd[0] - read
 	fd[1] - write
+
+	PIPE? -> communication between processes :)
+	whenever we write or read, we should check for the errors
+
 */
 
 int	main(int argc, char *argv[])
 {
 	int	fd[2];
 	int	id;
+	int	x;
+	int	y;
 
 	if (pipe(fd) == -1)
 	{
@@ -42,20 +48,31 @@ int	main(int argc, char *argv[])
 		return (1);
 	}
 	id = fork();
+	if (id == -1)
+	{
+		printf("An error ocurred with fork\n");
+		return (4);
+	}
 	if (id == 0)
 	{
 		close(fd[0]);
-		int x;
 		printf("Input number: ");
 		scanf("%d", &x);
-		write(fd[1], &x, sizeof(int));
+		if (write(fd[1], &x, sizeof(int)) == -1)
+		{
+			printf("An error ocurred with the writing to the pipe\n");
+			return (2);
+		}
 		close(fd[1]);
 	}
 	else
 	{
 		close(fd[1]);
-		int	y;
-		read(fd[0], &y, sizeof(int));
+		if (read(fd[0], &y, sizeof(int)) == -1)
+		{
+			printf("An error ocurred with the reading from the pipe\n");
+			return (3);
+		}
 		close(fd[0]);
 		printf("The parent got from child process %d\n", y);
 	}
