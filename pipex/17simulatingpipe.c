50af6282954e15f856da3ba08cc4ab0b0d049ae0
Denis Lom <lom.denis@seznam.cz>
2023-08-24 13:30:28 +0200

    simulating pipe ping | grep

--- pipex/17simulatingpipe.c
+++ pipex/17simulatingpipe.c
@@ -6,7 +6,7 @@
 /*   By: dlom <dlom@student.42prague.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 12:54:23 by dlom              #+#    #+#             */
-/*   Updated: 2023/08/24 13:02:41 by dlom             ###   ########.fr       */
+/*   Updated: 2023/08/24 13:29:40 by dlom             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,38 @@ pipe operator: |
 
 int	main(int argc, char *argv[])
 {
+	int	fd[2];
+	int	pid1;
+	int	pid2;
+
+	if (pipe(fd) == -1)
+		return (1);
+	pid1 = fork();
+	if (pid1 < 0)
+		return (2);
+	if (pid1 == 0)
+	{
+		// Child process 1: ping
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("ping", "ping", "-c", "5", "google.com", NULL);
+	}
+	pid2 = fork();
+	if (pid2 < 0)
+		return(3);
+	if (pid2 == 0)
+	{
+		// Child process 2: grep
+		dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execlp("grep", "grep", "rtt", NULL);
+	}
 	
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 	return (0);
 }
